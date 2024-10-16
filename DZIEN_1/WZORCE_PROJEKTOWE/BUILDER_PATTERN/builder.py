2868533824afccfeb2785b2706d7c67d228f7991
albim72 <marcin.albiniak72@gmail.com>
2024-10-16 11:47:14 +0200

    Create builder.py

---
+++ DZIEN_1/WZORCE_PROJEKTOWE/BUILDER_PATTERN/builder.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Builder(ABC):
+    @property
+    @abstractmethod
+    def product(self):
+        pass
+    
+    @abstractmethod
+    def produce_part_a(self):
+        pass
+
+    @abstractmethod
+    def produce_part_b(self):
+        pass
+
+    @abstractmethod
+    def produce_part_c(self):
+        pass
