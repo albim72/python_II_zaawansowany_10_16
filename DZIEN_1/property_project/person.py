0d4d64f5efa838353b556336cb6d2ab83f127d77
albim72 <marcin.albiniak72@gmail.com>
2024-10-16 10:10:06 +0200

    Create person.py

---
+++ DZIEN_1/property_project/person.py
@@ -0,0 +1,10 @@
+from datetime import datetime,time
+
+class Person:
+    def __init__(self,name,year,city):
+        self._name = name
+        self._year = year
+        self.city = city
+        self._desc = "uczestnik konkursu"
+        self.howmany = 9
+
