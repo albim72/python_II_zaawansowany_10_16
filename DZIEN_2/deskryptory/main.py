d61ba4a1c3f13249ccd7ad584f2b618393b72498
albim72 <marcin.albiniak72@gmail.com>
2024-10-17 15:37:10 +0200

    Update main.py

--- DZIEN_2/deskryptory/main.py
+++ DZIEN_2/deskryptory/main.py
@@ -1,5 +1,6 @@
 from desc1 import Person
 from desc2 import MClass
+from desc3 import MojLog
 
 #przykład desc1
 print("przykład desc1")
@@ -21,3 +22,28 @@ try:
     ob.constant = 900
 except AttributeError as ae:
     print(ae)
+
+
+#przykład desc3
+print("przykład desc3")
+
+try:
+    print("_"*50)
+    trzeci = MojLog()
+    trzeci.x = 10
+    print(trzeci.x)
+
+    print("_"*50)
+    del trzeci.x
+    print(trzeci.x)
+
+    print("_" * 50)
+    trzeci.y = "ładna jesień"
+    print(trzeci.y)
+
+    print("_"*50)
+    del trzeci.y
+    print(trzeci.y)
+
+except Exception as exc:
+    print(exc)
